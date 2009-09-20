f5df56fac83ca8f7c409bb6d0cbcbb3b945049a4
atom <re5etsmyth@gmail.com>
2009-09-19 23:11:30 -0400

    added mcrypt support

--- includes/fake_session.php
+++ includes/fake_session.php
@@ -8,18 +8,33 @@ class fake_session{
 	var $domain		=	'';
 	var $secure		=	false;
 	var $httponly	=	false;
+	var $mcrypt_key	=	'someUNIQUEstringSETbyYou'; // set to false to disable mcrypt encryption
+	var $mcrypt_iv	=	false;
 	
 	var $data		=	array();
 	
 	function __construct($options = array())
 	{
 		if(!empty($options))
+		{
 			$this->set_options($options);
-			
+		}
+		
+		if(function_exists('mcrypt_encrypt') && $this->mcrypt_key)
+		{
+			$this->mcrypt_key = md5($this->mcrypt_key);
+			$this->mcrypt_iv = mcrypt_create_iv(32);
+		}
+		
 		if($this->check_for_cookie())
+		{
 			$this->data = $this->load();
+		}
 		else
+		{
+			$this->save();
 			$this->set_cookie();
+		}
 	}
 	
 	function set_options($options)
@@ -40,7 +55,7 @@ class fake_session{
 	
 	function set_cookie($value = '')
 	{
-		setcookie(
+			setcookie(
 			$this->name,
 			$value,
 			time() + $this->expires,
@@ -53,12 +68,22 @@ class fake_session{
 	
 	function load()
 	{
-		return unserialize(base64_decode($_COOKIE[$this->name]));
+		$data = base64_decode($_COOKIE[$this->name]);
+		if(function_exists('mcrypt_encrypt') && $this->mcrypt_key)
+		{
+			$data = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->mcrypt_key, $data, MCRYPT_MODE_ECB, $this->mcrypt_iv);
+		}		
+		return unserialize($data);
 	}
 	
 	function save()
 	{
-		$this->set_cookie(base64_encode(serialize($this->data)));
+		$data = serialize($this->data);
+		if(function_exists('mcrypt_encrypt') && $this->mcrypt_key)
+		{
+			$data = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->mcrypt_key, $data, MCRYPT_MODE_ECB, $this->mcrypt_iv);
+		}
+		$this->set_cookie(base64_encode($data));
 	}
 
 	function __set($key, $value)
@@ -81,14 +106,28 @@ class fake_session{
 	
 	function delete()
 	{
-		setcookie ($this->name, "", time() - 3600);
-		$this->save();
+		setcookie($this->name, "", time() - 3600);
 	}
 	
 	function dump()
 	{
 		$this->data = array();
 		$this->save();
+	}
+	
+	function mcrypt_test()
+	{
+		if(function_exists('mcrypt_encrypt'))
+		{
+			die('You have Mcrypt.  The data stored in your cookie will be encrypted.
+			Make sure you use a unique string for your $mcrypt_key variable.');
+		}
+		else
+		{
+			die('You do not have Mcrypt.  The data stored in your cookie will not be encrypted.  
+			You should not use this store anything that you wish to be kept a secret.  
+			You can learn more about Mcrypt and how to install it here: http://php.net/mcrypt');
+		}
 	}
 	
 }
